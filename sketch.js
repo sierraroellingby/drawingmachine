ee197b177bcb26f738042c6575342ae34aa0d351
SierraRoe <sierra.roe@sjsu.edu>
2019-10-07 21:46:31 -0700

    blob umbrella

--- sketch.js
+++ sketch.js
@@ -1,18 +1,17 @@
 function setup() {
   createCanvas(400, 400);
+    background(245, 225, 174)
+    strokeWeight(7)
 }
 
 function draw() {
-colorMode(HSB);
-  background(230, 150, 80)
-  strokeWeight(7)
-
 
 
 if (mouseIsPressed){
 
+
+stroke(map(mouseX, 400, 100, true));
 line(mouseX, mouseY, pmouseX, pmouseY);
-stroke(map(mouseX, 400, 600, 200, 255, true));
 };
 
 }
@@ -21,4 +20,18 @@ function keyTyped(){
   saveCanvas('fileName', 'png')
 }
 return false
+
+beginShape();
+  fill(255, 255, 0);
+curveVertex(width * 0.2, height * 0.3);
+curveVertex(width * 0.65, height * 0.23);
+curveVertex(width * 0.8, height * 0.7);
+curveVertex(width*0.65, height* 0.6);
+curveVertex(width * 0.5, height * 0.5);
+curveVertex(width * 0.3, height * 0.4);
+curveVertex(width * 0.8, height * 0.7);
+endShape();
+
+fill(255, 255, 0);
+rect(width * 0.5, height * 0.5, 20, 120);
 }
