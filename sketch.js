0baa515b950dbefe1adface8fef787615d506ae8
sierraroellingby <55562338+sierraroellingby@users.noreply.github.com>
2019-10-22 15:48:14 -0700

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -4,21 +4,36 @@
 //   img = loadImage('umbrella-clipart-transparent-18_78.jpg');
 // }
 let hongkong;
+let shake;
 
 function preload(){
 
-  hongkong = loadImage('hongkongcrop4.png')
+  hongkong4 = loadImage('hongkongcrop4.png')
+  hongkong1 = loadImage('hongkongcrop1.png')
+  hongkong2 = loadImage('hongkongcrop2.png')
+  hongkong3 = loadImage('hongkongcrop3.png')
+  prochina1 = loadImage('prochina1.png')
+  prochina2 = loadImage('prochina2.png')
+  prochina3 = loadImage('prochina3.png')
+  prochina11 = loadImage('prochina11.png')
 }
 const particles = [];
 
 function setup() {
   createCanvas(1500, 800);
-
+shake = new Shakey();
 }
 
 function draw() {
   background(10);
-  image(hongkong, 50, 50)
+  image(hongkong4, 300, 50)
+  image(hongkong1, 30, 300)
+  image(hongkong2, 100, 500)
+  image(hongkong3, 600, 400)
+  image(prochina1, 1000, 100)
+  image(prochina2, 800, 50)
+  image(prochina3, 1300, 500)
+  image(prochina11, 950, 250)
   for (let i = 0; i < 5; i++) {
     // change last two parameters to change speed
     let p = new Particle(mouseX, mouseY, map(mouseX - pmouseX, 0, 10, -.5, .5), map(mouseY - pmouseY, 0, 10, .5, 1));
@@ -32,7 +47,6 @@ function draw() {
       particles.splice(i, 1);
     }
   }
-
 }
 //insert umbrella images
 
@@ -68,4 +82,21 @@ class Particle {
     ellipse(this.x - 30, this.y - 15, 15);
   }
 
+}
+class Shakey {
+  constructor() {
+    this.x = random(200);
+    this.y = random(200);
+    this.diameter = random(10, 30);
+    this.speed = 1;
+  }
+
+  move() {
+    this.x += random(-this.speed, this.speed);
+    this.y += random(-this.speed, this.speed);
+  }
+
+  show() {
+    ellipse(this.x, this.y, this.diameter, this.diameter);
+  }
 }
